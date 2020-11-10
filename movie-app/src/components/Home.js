19b83aa26fa6791055b5136e26b7abe9eca70494
bsihame <sihamebazi@pursuit.org>
2020-11-10 18:16:42 -0500

    add fetch movies function in the Home js file

--- movie-app/src/components/Home.js
+++ movie-app/src/components/Home.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  API_URL,
+  API_KEY,
+  API_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../config"; 
 import HeroImage from "./elements/HeroImage";
 import SearchBar from "./elements/SearchBar";
 import Grid from "./elements/Grid";
@@ -7,6 +14,37 @@ import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
 const Home = () => {
+  const [ state, setState ] = useState({ movies: [] });
+  const [ loading, setLoading ] = useState(false);
+  const [error, setError] = useState(false);
+
+  console.log(state)
+
+  const fetchMovies = async endpoint => {
+    setError(false);
+    setLoading(true);
+
+    try {
+      const result = await (await fetch(endpoint)).json();
+      console.log(result)
+      setState(prev => ({
+        ...prev,
+        movies: [...result.results],
+        HeroImage: prev.HeroImage || result.results[0],
+        currentPage: result.page,
+        totalPages: result.total_pages,
+      }))
+
+    } catch (error) {
+      setError(true);
+      console.log(error)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+  },[])
   return (
     <>
       <HeroImage />
