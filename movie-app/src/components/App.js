61175df175deaedad2679d109faaa9ecca0d653c
bsihame <sihamebazi@pursuit.org>
2020-11-10 10:15:36 -0500

    add Headers components

---
+++ movie-app/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from "../components/elements/Header"
+
+const App = () => {
+
+  return(
+    <div><Header />Start Here!</div>
+  )
+};
+
+export default App;
